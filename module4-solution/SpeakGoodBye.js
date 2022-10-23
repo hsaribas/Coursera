dce38f0f9218de16ecfe4b7421e42030f78973fe
Huzeyfe Saribas <huzeyfeorhan77@gmail.com>
2022-10-23 22:21:12 +0300

    third commit

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function (window) {
+  var goodByeSpeaker = {};
+  var speakWord = "Good Bye";
+  goodByeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+  window.goodByeSpeaker = goodByeSpeaker;
+})(window);
